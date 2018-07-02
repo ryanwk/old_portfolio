1803710d02c93815b0e418003ee84f51bc82f44f
ryan <ryanwkoch@gmail.com>
2018-07-02 13:49:12 -0400

    add.sh script to upload to bucket. Removed files that were not necessary

---
+++ add.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+gulp
+
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./css gs://www.ryanwkoch.com/css/
+
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./img gs://www.ryanwkoch.com/img/
+
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./lib gs://www.ryanwkoch.com/lib/
+
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./index.html gs://www.ryanwkoch.com
+
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./e-commerce.html gs://www.ryanwkoch.com
+
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./stronger.html gs://www.ryanwkoch.com
+
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./tic-tac-toe.html gs://www.ryanwkoch.com
+
+
+gsutil iam ch allUsers:objectViewer gs://www.ryanwkoch.com
