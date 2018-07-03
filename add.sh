dc845d7d6c4282493443484b4b7194ec776df3c5
ryan <ryanwkoch@gmail.com>
2018-07-03 15:21:01 -0400

    changing icons of contact section and updating landing page photo (hero-bg.jpg)

--- add.sh
+++ add.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
-gulp
+#gulp
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./css gs://www.ryanwkoch.com/css/
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./css gs://www.ryanwkoch.com/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./img gs://www.ryanwkoch.com/img/
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./img gs://www.ryanwkoch.com/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./lib gs://www.ryanwkoch.com/lib/
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./lib gs://www.ryanwkoch.com/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./index.html gs://www.ryanwkoch.com
+gsutil -h "Cache-Control:public, max-age=2592000" cp -r -Z ./js gs://www.ryanwkoch.com/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./e-commerce.html gs://www.ryanwkoch.com
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./index.html gs://www.ryanwkoch.com/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./stronger.html gs://www.ryanwkoch.com
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./e-commerce.html gs://www.ryanwkoch.com/
 
-gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./tic-tac-toe.html gs://www.ryanwkoch.com
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./stronger.html gs://www.ryanwkoch.com/
+
+gsutil -h "Cache-Control:public, max-age=2592000" cp -Z ./tic-tac-toe.html gs://www.ryanwkoch.com/
 
 
 gsutil iam ch allUsers:objectViewer gs://www.ryanwkoch.com
