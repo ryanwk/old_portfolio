09ab53bd624840ef49c8432b9c3f21222855693a
ryan <ryanwkoch@gmail.com>
2019-04-03 21:48:34 -0700

    remove all old portfolio items

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-
-// var beautify = require('gulp-beautify');
-
-// gulp.task('beautify', function () {
-//     gulp.src('./js/*.js')
-//         .pipe(beautify({ indent_size: 2 }))
-//         .pipe(gulp.dest('./public/'))
-// });
